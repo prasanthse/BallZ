5079d5eee5d6267523077c86eb0f752e6ad4208a
Prasanth <31370970+prasanthse@users.noreply.github.com>
2019-04-19 15:52:39 +0530

    Loading scene finished and logo added

---
+++ Assets/Scripts/AutoSceneChange.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AutoSceneChange : MonoBehaviour{
+
+    private float timer = 3;
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -1,16 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int sceneNumber;
+
+    public void OnClickSceneChange(){
+        SceneManager.LoadScene(sceneNumber);
+    }
 }
