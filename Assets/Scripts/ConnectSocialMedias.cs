3cbbb86eaa67e9c573e2566576ed0d8d1d1ea81c
Prasanth <31370970+prasanthse@users.noreply.github.com>
2019-04-21 10:14:46 +0530

    help option finished

---
+++ Assets/Scripts/ConnectSocialMedias.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConnectSocialMedias : MonoBehaviour{
+
+    public void connectMyFb(){
+        Application.OpenURL("https://www.facebook.com/profile.php?id=100005272362325");
+    }
+
+    public void connectMyLinkedIn(){
+        Application.OpenURL("https://www.linkedin.com/in/prasanthse1996/");
+    }
+
+}
