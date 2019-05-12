0eb8778f32ad90957c3ae1a32fb1d76136a59edb
Prasanth <31370970+prasanthse@users.noreply.github.com>
2019-05-12 11:37:24 +0530

    Cracked Hole finished

--- Assets/Scripts/DestroyObject.cs
+++ Assets/Scripts/DestroyObject.cs
@@ -4,11 +4,41 @@ using UnityEngine;
 
 public class DestroyObject : MonoBehaviour
 {
+    public string objectTag;
+    public GameObject hole;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag.Equals("Star"))
+        if (other.gameObject.tag.Equals(objectTag) || other.gameObject.tag.Equals("Hole"))
         {
             Destroy(other.gameObject);
+
+            if(objectTag == "LavaRock")
+            {
+                InitiateHole(other);
+            }
+        }
+    }
+
+    private void InitiateHole(Collider rock)
+    {
+        if(rock.transform.position.x < 1.5 && rock.transform.position.x > -1.5)
+        {
+            if(rock.transform.position.x > 1.25 || rock.transform.position.x < -1.25)
+            {
+                if(rock.transform.position.x > 1.25)
+                {
+                    Instantiate(hole, new Vector3((float)1.24, (float)0.03, rock.transform.position.z), Quaternion.Euler(90, 0, 0));
+                }
+                else
+                {
+                    Instantiate(hole, new Vector3((float)-1.24, (float)0.03, rock.transform.position.z), Quaternion.Euler(90, 0, 0));
+                }
+            }
+            else
+            {
+                Instantiate(hole, new Vector3(rock.transform.position.x, (float)0.03, rock.transform.position.z), Quaternion.Euler(90, 0, 0));
+            }
         }
     }
 }
